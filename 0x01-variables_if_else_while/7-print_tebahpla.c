e5018a012b2f07f8ea6f96b5492309a1c375d67c
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-09 12:49:27 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 means success
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
