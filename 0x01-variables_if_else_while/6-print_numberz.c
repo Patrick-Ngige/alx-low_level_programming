f38fed7d16424441d41a15a5acc8904c49dfb60a
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-09 12:43:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 means success
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
