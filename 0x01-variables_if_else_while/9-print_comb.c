c2356e5cd81bb23ab1edbae3f067ac59436d71e7
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-09 13:04:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c < '9')
+{
+putchar(',');
+putchar(' ');
+}
+else
+putchar('\n');
+}
+return (0);
+}
