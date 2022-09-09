66f5b8a2410e89a3136ee22a6b1024d42ee235dc
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-09 13:23:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,12 @@ int main(void)
 int i;
 for (i = 0; i < 100; i++)
 {
+if ((i / 10) < (i % 10))
+{
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
-if (i < 99)
+}
+if (i < 89 && (i / 10) < (i % 10))
 {
 putchar(',');
 putchar(' ');
