5939167ea12ad6a8eb0c81295934c32afbb23f0f
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-16 14:08:14 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,8 @@ int a;
 for (a = 1; a <= 100; a++)
 {
 ((a % 3) == 0) ?
-(((a % 5) == 0) ? (printf("FizzBuzz")) : (printf("Fizz"))) : (((a % 5) == 0) ? (printf("Buzz")) : (printf("%i", a)));
+(((a % 5) == 0) ? (printf("FizzBuzz")) : (printf("Fizz"))) :
+(((a % 5) == 0) ? (printf("Buzz")) : (printf("%i", a)));
 (a < 100) ? (putchar(' ')) : (putchar('\n'));
 }
 return (0);
