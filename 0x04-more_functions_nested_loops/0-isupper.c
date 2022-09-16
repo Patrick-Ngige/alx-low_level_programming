e978533e0970b3ca7aae45c7b4d2eda72433af8c
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-16 14:15:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "_putchar.c"
+
 /**
  * _isupper - checks whether the character is upper
  *@c:the character input
@@ -9,10 +9,11 @@ int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
-_putchar(1);
+return (1);
 }
 else
 {
-_putchar(0);
+return (0);
+}
 _putchar('\n');
 }
