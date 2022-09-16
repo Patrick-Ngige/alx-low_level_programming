e23488e65d535d0bd991b7faf91ce825a395bf83
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-16 13:06:23 -0700

    8-square.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -32,13 +32,13 @@ void print_triangle(int size);
 
 void print_number(int n);
 
-int _putchar(char);
+int _putchar(char c);
 
-int _isupper(int);
+int _isupper(int c);
 
-int _isdigit(int);
+int _isdigit(int c);
 
-int mul(int, int);
+int mul(int a, int b);
 
 
 
