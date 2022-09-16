69a0fe81947df7e5fcf7aa5a741220a49b1aa2ee
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-16 13:19:43 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - outputs a line
+ * @n: input to check
+ * Return: no return
+ */
+void print_line(int n)
+{
+for (; n > 0; n--)
+{
+_putchar('_');
+_putchar('\n');
+}
