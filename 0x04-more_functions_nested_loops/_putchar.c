b5557ca957f9859756f468f0cebc03bd968acaa5
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-16 11:59:56 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+/**
+ * _putchar - writees the character c to stdout
+ * !c: the character to print
+ * Return: on success 1.
+ * on error, -1.
+ */
+ int _putchar(char c)
+ return (write(1, &c, 1));
