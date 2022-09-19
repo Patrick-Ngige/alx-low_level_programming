0c84138e1caa5f988d89517b80223bf3c12fd5db
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-19 05:55:50 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - functions to calculate the lentgh of a string
+ * @n: string to be measured
+ * Return: empty
+ */
+int _strlen(char *n)
+{
+int count;
+for (count = 0; n[count] != '\0'; count++)
+{
+}
+return (count);
+}
