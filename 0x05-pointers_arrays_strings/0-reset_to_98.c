237f2580147bd7cdc71460a53b5fc085272645ee
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-19 05:22:16 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - acts as main function
+ * @n: acts as a pointer
+ * Return: no expected return
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,57 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+/**
+ *
+ * *main - header file for protoypes
+ *
+ * *void protoypes (void)
+ *
+ * *int protoypes (int)
+ *
+ * */
+
+
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void reset_to_98(int *n);                                                                                                     
+
+void swap_int(int *a, int *b);                                                                                                                           
+int _strlen(char *s);                                                                                                         
+
+void _puts(char *str);                                                                                                        
+
+void print_rev(char *s);                                                                                                      
+                                                                                                                              
+void rev_string(char *s);                                                                                                     
+                                                                                                                              
+void puts2(char *str);      
+
+
+#endif /* MAIN_H */
