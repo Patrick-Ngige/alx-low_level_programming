565d306c1b0d3389e49ab6656b004d16fb94a48d
Patrick Ngige <wakemanjajr@gmail.com>
2022-09-22 05:46:37 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,46 +1,34 @@
 #include "main.h"
 
 /**
- * is_sep - checks to see if the char is a separator or not
- * @n: a chaacter to test
- * Return: 1 if separator and 0 if not
+ * cap_string - Capitalizes all characters of a string
+ * @str: the string to be capitalized
+ * Return: A pointer to the changed string
  */
-int is_sep(char n)
+char *cap_string(char *str)
 {
-	char sep[] = {' ', '\t', '\n', ',', ';', '!', '?', '"', '(', ')', '{', '}'};
+	int index = 0;
 
-	int i = 0;
-
-	while (sep[i] != '\0')
+	while (str[index])
 	{
-		if (n == sep[i])
-			return (1);
-		i++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (
+				str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
 	}
-	return (0);
-}
-
-/**
- * cap_string - capitalizes all the characters of the given string
- * @n: the string to be tested
- * Return: capitalized string
- */
-char *cap_string(char *n)
-{
-	int sep, i;
-
-	sep = 1;
-	i = 0;
-
-	while (n[i] != '\0')
-	{
-		if (sep == 1 && (n[i] >= 'a' && n[i] <= 'z'))
-		{
-			n[i] -= 32;
-			sep = 0;
-		}
-		sep = is_sep(n[i]);
-		i++;
-	}
-	return (n);
+	return (str);
 }
