222991c08c501f3c146a207d6d1cf0fb9318218d
Patrick Ngige <wakemanjajr@gmail.com>
2022-10-26 12:40:20 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t n_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		n_nodes++;
 	}
-	return (count);
+	return (n_nodes);
 }
